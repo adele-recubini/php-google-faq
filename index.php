86bdfe40076e04416cdf3a2d2be28b291682a420
adele <adele.r87@gmail.com>
2021-03-01 15:28:49 +0100

    modifiche

--- index.php
+++ index.php
@@ -38,11 +38,18 @@ $google =[
     Stabilire il paese associato al tuo account
    Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
 
-   I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
+   I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per  aggiornare l'associazione del paese.
 
    Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato."
 
-  ]
+ ],
+ [
+   "faq" =>"Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+   "ansewer" =>"Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?
+I risultati di ricerca di Google rispecchiano i contenuti pubblicamente  disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
+
+
+ ]
 
 ];
 
@@ -69,7 +76,7 @@ $google =[
       </ul>
 
       <?php } ?>
-
+      <hr>
 
      </div>
 
